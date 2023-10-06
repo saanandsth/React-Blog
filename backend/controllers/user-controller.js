77426c0f687ce438209a0b0f7fa200441f15b015
saanandsth <Sthavara.Saanand1@ltimindtree.com>
2023-10-06 16:00:49 +0530

    get req and post req

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,5 +1,6 @@
 import User from '../models/User';
 
+// it is being called in user-router.js
 export const getAllUsers = async (req, res, next) => {
   let users;
   try {
@@ -12,4 +13,29 @@ export const getAllUsers = async (req, res, next) => {
     return res.status(404).json({ message: 'No Records Found' });
   }
   return res.status(200).json({ users });
+};
+
+export const signup = async (req, res, next) => {
+  const { name, email, password } = req.body;
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (err) {
+    console.log(err);
+  }
+
+  if (existingUser) {
+    return res.status(400).json({ message: 'User Already Exists' });
+  }
+  const user = new User({
+    name,
+    email,
+    password,
+  });
+  try {
+    await user.save();
+  } catch (error) {
+    console.log(error);
+  }
+  return res.status(201).json({ user });
 };
