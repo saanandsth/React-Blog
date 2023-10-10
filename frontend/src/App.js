e7356796ab1625970d8900cd3e7ab9e565f117b6
saanandsth <Sthavara.Saanand1@ltimindtree.com>
2023-10-11 00:40:43 +0530

    added react-router-dom and added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
+import React from 'react';
 import Header from './components/Header';
+import { Route, Routes } from 'react-router-dom';
+import Auth from './components/Auth';
+import Blogs from './components/Blogs';
+import UserBlogs from './components/UserBlogs';
+import BlogDetail from './components/BlogDetail';
+import AddBlog from './components/AddBlog';
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <main>
+        <Routes>
+          <Route path='/auth' element={<Auth />}></Route>
+          <Route path='/blogs' element={<Blogs />} />
+          <Route path='/blogs/add' element={<AddBlog />} />
+          <Route path='/myBlogs' element={<UserBlogs />} />
+          <Route path='/myBlogs/:id' element={<BlogDetail />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,12 +4,27 @@ import { AppBar, Button, Toolbar, Typography, Box } from '@mui/material';
 const Header = () => {
   return (
     <>
-      <AppBar sx={{ background: 'green' }}>
+      <AppBar
+        position='sticky'
+        sx={{
+          background:
+            'linear-gradient(90deg, rgba(238,41,53,1) 20%, rgba(6,65,78,1) 76%)',
+        }}>
         <Toolbar>
           <Typography variant='h4'>Blogs App</Typography>
-          <Box>
-            <Button color='warning'>Login</Button>
-            <Button color='warning'>Sign Up</Button>
+          <Box display='flex' marginLeft='auto'>
+            <Button
+              color='warning'
+              variant='info'
+              sx={{ margin: 1, borderRadius: 10 }}>
+              Login
+            </Button>
+            <Button
+              color='warning'
+              variant='info'
+              sx={{ margin: 1, borderRadius: 10 }}>
+              Sign Up
+            </Button>
           </Box>
         </Toolbar>
       </AppBar>
