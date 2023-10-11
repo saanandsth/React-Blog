7cddb3bafcbd8806348a3a10c46c451c0bfca991
saanandsth <Sthavara.Saanand1@ltimindtree.com>
2023-10-11 12:17:13 +0530

    configured routes for header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,17 @@
-import React from 'react';
-import { AppBar, Button, Toolbar, Typography, Box } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  AppBar,
+  Button,
+  Toolbar,
+  Typography,
+  Box,
+  Tabs,
+  Tab,
+} from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const [value, setValue] = useState();
   return (
     <>
       <AppBar
@@ -12,18 +22,40 @@ const Header = () => {
         }}>
         <Toolbar>
           <Typography variant='h4'>Blogs App</Typography>
+          <Box display='flex'>
+            <Tabs
+              sx={{ margin: 1 }}
+              textColor='inherit'
+              value={value}
+              onChange={(e, val) => setValue(val)}>
+              <Tab LinkComponent={Link} to='/blogs' label='All Blogs'></Tab>
+              <Tab LinkComponent={Link} to='/myBlogs' label='My Blogs'></Tab>
+            </Tabs>
+          </Box>
           <Box display='flex' marginLeft='auto'>
             <Button
+              LinkComponent={Link}
+              to='/auth'
               color='warning'
               variant='info'
               sx={{ margin: 1, borderRadius: 10 }}>
               Login
             </Button>
             <Button
+              LinkComponent={Link}
+              to='/auth'
               color='warning'
               variant='info'
               sx={{ margin: 1, borderRadius: 10 }}>
               Sign Up
+            </Button>
+            <Button
+              LinkComponent={Link}
+              to='/auth'
+              color='warning'
+              variant='info'
+              sx={{ margin: 1, borderRadius: 10 }}>
+              Logout
             </Button>
           </Box>
         </Toolbar>
