e9e0e33f8ab199033c11972655b3a973db65c6d3
saanandsth <Sthavara.Saanand1@ltimindtree.com>
2023-10-12 15:18:51 +0530

    added blog cards and minor fixes

---
+++ frontend/src/components/Blog.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+
+const Blog = ({ title, desc, image, user }) => {
+  return (
+    <>
+      <Card
+        sx={{
+          width: '20%',
+          margin: '10px',
+          marginTop: 2,
+          padding: 2,
+          boxShadow: '5px 5px 10px #ccc',
+          ':hover': {
+            boxShadow: '10px 10px 20px #ccc',
+          },
+        }}>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: red[500] }} aria-label='recipe'>
+              {user}
+            </Avatar>
+          }
+          title={title}
+          // subheader='September 14, 2016'
+        />
+        <CardMedia component='img' height='194' image={image} alt='img' />
+        <CardContent>
+          <Typography variant='body2' color='text.secondary'>
+            {desc}
+          </Typography>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
+
+export default Blog;
--- frontend/src/components/Blogs.js
+++ frontend/src/components/Blogs.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Blog from './Blog';
+import { Stack } from '@mui/system';
 
 const Blogs = () => {
   const [blogs, setBlogs] = useState();
@@ -12,10 +14,26 @@ const Blogs = () => {
       .get('http://localhost:5000/api/blog')
       .catch((err) => console.log(err));
     const data = await res.data;
+    console.log('data', data);
     return data;
   };
 
-  return <div>Blogs</div>;
+  return (
+    <div>
+      <Stack direction='row' spacing={2} sx={{ margin: '20px' }}>
+        {blogs &&
+          blogs?.map((blog, index) => (
+            <Blog
+              title={blog.title}
+              desc={blog.description}
+              image={blog.image}
+              key={index}
+              user={blog.user.name}
+            />
+          ))}
+      </Stack>
+    </div>
+  );
 };
 
 export default Blogs;
